1c38b68c442747bab2ca4eaab27a786382955981
Willie  Conway <85038615+Willie-Conway@users.noreply.github.com>
2024-10-31 21:29:14 -0400

    Delete Datasources directory

--- Datasources/Load_file_csv.sql
+++
@@ -1,7 +0,0 @@
-SET GLOBAL local_infile = 'ON';
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Global_Superstore_DB.csv'
-INTO TABLE Orders
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
